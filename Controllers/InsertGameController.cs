4d77e27dcb3e0abdf17f9dba9b9ea6a2f9af6514
Eduardo Ordakowski <eduardoordakowski@bne.com.br>
2025-09-02 21:54:54 -0300

    Fix: Corrigindo erro da lista de jogos + Mensagens de erro no formulário

--- Controllers/InsertGameController.cs
+++ Controllers/InsertGameController.cs
@@ -45,7 +45,7 @@ namespace QuestKeeper2._0.Controllers
         {
             var NewGame = Game;
             
-            if(Game == null)
+            if(!ModelState.IsValid)
             {
                 throw new Exception("Campos não foram preenchidos devidamente");
             }
@@ -53,7 +53,7 @@ namespace QuestKeeper2._0.Controllers
             _db.Game.Add(NewGame);
             await _db.SaveChangesAsync();
 
-            return View("Index");
+            return RedirectToAction("Index");
         }
 
         [HttpPost]
@@ -75,7 +75,7 @@ namespace QuestKeeper2._0.Controllers
             _db.Game.Update(GameToUpdate);
             await _db.SaveChangesAsync();
 
-            return View("Index");
+            return RedirectToAction("Index");
         }
     }
 }
--- Models/GameModel.cs
+++ Models/GameModel.cs
@@ -1,4 +1,6 @@
-﻿namespace QuestKeeper2._0.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QuestKeeper2._0.Models
 {
     public class GameModel
     {
@@ -6,9 +8,18 @@
         public int Id { get; set; }
         public string BannerImgUrl { get; set; }
         public string IconImg { get; set; }
+        
+        [Required (ErrorMessage = "Preencha o campo de nome!")]
         public string Name { get; set; }
+        
+        [Required (ErrorMessage = "Preencha o campo de genero!")]
         public string Genre { get; set; }
+        
+        [Required (ErrorMessage = "Preencha o campo de descrição!")]
         public string Description { get; set; }
+        
+        [Required (ErrorMessage = "Preencha o campo de desenvolvedor!")]
+        
         public string Developer { get; set; }
         public DateTime RealeseDate { get; set; }
 
